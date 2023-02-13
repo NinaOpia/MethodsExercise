62f440b33a2b3d2737b71f97ff95028bb38e2cef
Nina <boldurfaith@gmail.com>
2023-02-12 23:24:01 -0800

    initial

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MethodsExercise
+{
+	public class Methods
+	{
+		public static int Add(int num1, int num2)
+        {
+			int sum = num1 + num2;
+			return sum;
+			 
+		}
+	}
+}
+
