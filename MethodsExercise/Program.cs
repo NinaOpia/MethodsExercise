6ae8e6b86060b36e239dd116dde1dbd01295711e
Nina <boldurfaith@gmail.com>
2023-03-08 07:58:47 -0800

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,35 +1,96 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+        //Exercise 1
+
+        public static void MadLib()
         {
-            ////Exercise 1
-            //Name: Michael
-            //Favorite Color: Blue
-            //Favorite Animal: Walrus
-            //Favorite Band: The Beatles
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
 
-            Console.WriteLine("Hello - What is your first name?"); //output
-            var userName = Console.ReadLine(); //input
+            //string interpolation with $
+            Console.WriteLine($"Hi, {userName}, what is your favorite color?");
+            var faveColor = Console.ReadLine();
 
-            Console.WriteLine($"Hi, {userName}. What is your favorite color?"); //output
-            var color = Console.ReadLine(); //input
+            Console.WriteLine($"{faveColor} is an awesome color! What is your favorite animal?");
+            var faveAnimal = Console.ReadLine();
 
-            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?"); //output
-            var animal = Console.ReadLine(); //input
-
-            Console.WriteLine("Excellent! Now, what is your fvorite band?"); //output
-            var band = Console.ReadLine(); //input
+            Console.WriteLine("Excellent! Now, what is your fvorite band?");
+            var faveBand = Console.ReadLine();
 
             Console.WriteLine($"Thanks, {userName}! Here is your profile");
             Console.WriteLine("--------------------------------------");
 
             Console.WriteLine($"Name: {userName}");
-            Console.WriteLine($"Favorite color: {color}");
-            Console.WriteLine($"Favorite Animal: {animal}");
-            Console.WriteLine($"Favorite Band: {band}");
+            Console.WriteLine($"Favorite Color: {faveColor}");
+            Console.WriteLine($"Favorite Animal: {faveAnimal}");
+            Console.WriteLine($"Favorite Band: {faveBand}");
+
+            Console.WriteLine($"One day, {userName} was going to the Vet clinic wearing a {faveColor} dress." +
+            $"On getting there, she saw a {faveAnimal} looking at her through one of the windows in the Vet clinic." +
+            $"As she walked into the Vet clinic, she heard her {faveBand} playing on the radio nearby. {userName} was amazed to be hearing that song in a Vet clinic.");
+
+        }
+
+
+        //Exercise 2
+        public static int Sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            int multiply = num1 * num2;
+
+            return num1 * num2;
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            int amountOfCars = Sum(2, 6);
+            int Samp = Multiply(60, 2);
+
+            Console.WriteLine("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is: {sum}");
+
+            Console.WriteLine("Give me a number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to multiply to the first one");
+            num2 = int.Parse(Console.ReadLine());
+
+            int product = Multiply(num1, num2);
+            Console.WriteLine($"The product is: {product}");
+        }
+
+
+        //Challenge mode using the params keyword:
+        public static int Multiply(params int[] nums)
+        {
+            int product = 1;
+
+            foreach (int x in nums)
+            {
+                product *= x;
+            }
+
+            return product;
 
         }
     }
 }
+        
